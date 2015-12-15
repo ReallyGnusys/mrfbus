16d8dbf0db36f2cba5bf1e09e487f5ae80f10a52
Steve Murphy <steve@gnusys.com>
2015-12-15 23:15:59 +0100

    amazing if we get it back now

---
+++ mrflib/src/mrf_sys_cmds.h
@@ -0,0 +1,23 @@
+#ifndef __MRF_SYS_CMDS_INCLUDED__
+#define __MRF_SYS_CMDS_INCLUDED__
+
+typedef enum mrf_cmd_code
+ {
+   mrf_cmd_ack = 0,
+   mrf_cmd_retry = 1,
+   mrf_cmd_resp = 2,
+   mrf_cmd_device_info = 3,
+   mrf_cmd_if_info = 4,
+   mrf_cmd_if_status = 5,
+   mrf_cmd_get_time = 6,
+   mrf_cmd_set_time = 7,
+   mrf_cmd_sensor_data = 8,
+   mrf_cmd_read_sensor = 9,
+   mrf_cmd_test_1 = 10, 
+   mrf_cmd_test_2 = 11
+ } MRF_CMD_CODE;
+
+#define MRF_NUM_SYS_CMDS 12
+
+
+#endif
