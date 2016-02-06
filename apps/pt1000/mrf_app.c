d2051864452cb22c68699b3eee568008ba365a68
Steve Murphy <steve@gnusys.com>
2016-02-06 21:58:44 +0100

    template copied for pt1000 app

---
+++ apps/pt1000/mrf_app.c
@@ -0,0 +1,20 @@
+#include "mrf_sys.h"
+#include <mrf_debug.h>
+
+
+MRF_CMD_RES mrf_task_usr_resp(MRF_CMD_CODE cmd,uint8 bnum, MRF_IF *ifp){
+  _mrf_buff_free(bnum);
+}
+
+
+MRF_CMD_RES mrf_app_task_test(MRF_CMD_CODE cmd,uint8 bnum, MRF_IF *ifp){
+  mrf_debug("mrf_app_task_test entry\n");
+  uint8 *rbuff = mrf_response_buffer(bnum);
+  mrf_rtc_get(rbuff);
+  mrf_send_response(bnum,sizeof(TIMEDATE));
+  mrf_debug("mrf_app_task_test exit\n");
+  return MRF_CMD_RES_OK;
+}
+
+
+
