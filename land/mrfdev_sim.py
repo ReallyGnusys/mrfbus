041459d372174c636ded8969a4115e5bcfc63855
Steve Murphy <steve@gnusys.com>
2017-07-12 23:16:33 +0200

    travis tests pass for me

--- land/mrfdev_sim.py
+++ land/mrfdev_sim.py
@@ -4,7 +4,7 @@ from datetime import datetime
 import ctypes
 from mrf_structs import *
 from core_tests import mrf_cmd_app_test, DefaultAppCmds
-
+import mrflog
 
     
         
@@ -15,7 +15,7 @@ class MrfDevSim(MrfDev):
     }                 
 
     def app_packet(self, hdr, param , resp):
-        self.log.warn("%s app_packet type %s"%(self.__class__.__name__, type(resp)))        
-        self.log.warn("hdr %s param %s resp %s"%(repr(hdr), repr(param), repr(resp)))
+        mrflog.warn("%s app_packet type %s"%(self.__class__.__name__, type(resp)))        
+        mrflog.warn("hdr %s param %s resp %s"%(repr(hdr), repr(param), repr(resp)))
         return
                 
--- land/test_server.py
+++ land/test_server.py
@@ -3,7 +3,7 @@
 from tornado.options import  options, parse_command_line  #yFIXME move mrfland_server.py
 
 import mrfland
-from mrflog import mrf_log_init, mrflog
+import mrflog
 from mrfdev_pt1000 import Pt1000Dev
 from mrfdev_heatbox import DevHeatbox
 from mrfdev_sim import MrfDevSim
@@ -12,20 +12,19 @@ from mrfland_server import MrflandServer
             
 if __name__ == '__main__':
     parse_command_line()
-    alog = mrf_log_init()
-    alog.info('Application started')
-    alog.info("Mrfland web server starting on port "+str(options.port))
+    mrflog.info('Application started')
+    mrflog.info("Mrfland web server starting on port "+str(options.port))
     
-    rm = mrfland.MrflandRegManager(alog)
+    rm = mrfland.MrflandRegManager()
 
 
 
     
-    sx01 = MrfDevSim(rm, "sx01", 1, {}, alog)
-    sx02 = MrfDevSim(rm, "sx02", 2, {}, alog)
-    sx20 = MrfDevSim(rm, "sx20", 0x20, {}, alog)
-    sxsf = MrfDevSim(rm, "sx2f", 0x2f, {}, alog)
+    sx01 = MrfDevSim(rm, "sx01", 1, {} )
+    sx02 = MrfDevSim(rm, "sx02", 2, {} )
+    sx20 = MrfDevSim(rm, "sx20", 0x20, {} )
+    sxsf = MrfDevSim(rm, "sx2f", 0x2f, {} )
     
-    ml =  MrflandServer(rm, alog )
+    ml =  MrflandServer(rm)
 
 
