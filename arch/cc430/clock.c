f6ff88d02920f38e054391b1978232acc71f45c7
Steve Murphy <steve@gnusys.com>
2017-08-24 19:04:17 +0200

    MCLK changes..possibly 8MHz now.. fixes missed acks

--- arch/cc430/clock.c
+++ arch/cc430/clock.c
@@ -71,7 +71,7 @@ void init_clock(void){
  UCSCTL4 |=  SELA__XT1CLK ;//+ SELM__XT1CLK + SELS__XT1CLK;
  __bis_SR_register(SCG0);                  // Disable the FLL control loop
  UCSCTL0 = 0x0000;                         // Set lowest possible DCOx, MODx
- UCSCTL1 = DCORSEL_7;                      // Select DCO range 16MHz operation
+ UCSCTL1 = DCORSEL_6;                      // Select DCO range 16MHz operation
  UCSCTL2 = FLLD_1 + 255;                   // Set DCO Multiplier for 8MHz
                                             // (N + 1) * FLLRef = Fdco
                                             // (249 + 1) * 32768 = 8MHz
@@ -112,7 +112,7 @@ void init_clock(void){
  UCSCTL4 |=  SELA__XT1CLK ;//+ SELM__XT1CLK + SELS__XT1CLK;
  __bis_SR_register(SCG0);                  // Disable the FLL control loop
  UCSCTL0 = 0x0000;                         // Set lowest possible DCOx, MODx
- UCSCTL1 = DCORSEL_5;                      // Select DCO range 16MHz operation
+ UCSCTL1 = DCORSEL_6;                      // Select DCO range 16MHz operation
  UCSCTL2 = FLLD_1 + 255;                   // Set DCO Multiplier for 8MHz
                                             // (N + 1) * FLLRef = Fdco
                                             // (249 + 1) * 32768 = 8MHz
@@ -136,7 +136,7 @@ void init_clock(void){
 
 
  // UCSCTL4 = SELA__REFOCLK + SELS__DCOCLKDIV + SELM__DCOCLKDIV;
- UCSCTL4 = SELA__XT1CLK + SELS__DCOCLKDIV + SELM__DCOCLKDIV;
+ UCSCTL4 = SELA__XT1CLK + SELS__DCOCLKDIV + SELM__DCOCLK;
 
 #endif
 #ifdef MCLK_4MHZ_DCO
