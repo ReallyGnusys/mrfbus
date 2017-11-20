a3e26703f0f83d1404cd3e4faff6cf8c518d7b2c
Steve Murphy <steve@gnusys.com>
2017-11-21 00:23:05 +0100

    motor testing

--- land/motor_tst.py
+++ land/motor_tst.py
@@ -29,48 +29,19 @@ def new_sensor_day_doc(sensor_id, docdate):
     
     return doc
 
-@gen.coroutine
-def do_insert_doc(**kwargs):
-
-    col = kwargs['collection']
-    doc = kwargs['doc']    
-
-    future = col.insert_one(doc)
-    result = yield future
-    print "result was %s"%repr(result)
-
-
-@gen.coroutine
-def do_update_doc(**kwargs):
-
-    col = kwargs['collection']
-    docdate = kwargs['docdate']    
-    minute = kwargs['minute']
-    hour = kwargs['hour']
-
-    future = col.update({'docdate':docdate},
-               { "$set" : { 'data.0.0' : 55.9 } })
-    #future = col.insert_one(doc)
-    result = yield future
-
-
-    if result['updatedExisting']:
-        mrflog.info("doc update result good %s "%repr(result))
-    else:
-        mrflog.info("result dodgy %s "%repr(result))
-        
         
 @gen.coroutine
 def do_insert(**kwargs):
 
     sensor_id = kwargs['sensor_id']
+    stype = kwargs['stype']
     docdate = kwargs['docdate']    
     minute = kwargs['minute']
     hour = kwargs['hour']
     value  = kwargs['value']
 
 
-    coll = db.get_collection('sensors.%s'%sensor_id)
+    coll = db.get_collection('sensor.%s.%s'%(stype,sensor_id))
 
     future = coll.update({'docdate':docdate},
                { "$set" : { 'data.0.0' : value } })
@@ -82,22 +53,33 @@ def do_insert(**kwargs):
         mrflog.warn("doc update result good %s "%repr(result))
     else:
         mrflog.warn("result dodgy %s "%repr(result))
+        doc = new_sensor_day_doc(sensor_id, docdate)
+        future = coll.insert_one(doc)
+        result = yield future
+        mrflog.warn("insert result was %s"%repr(result))
+        future = coll.update({'docdate':docdate},
+                             { "$set" : { 'data.0.0' : value } })
+        #future = col.insert_one(doc)
+        result = yield future
+        if result['updatedExisting']:
+            mrflog.warn("doc update(2) result good %s "%repr(result))
+        else:
+            mrflog.error("result(2) bad %s "%repr(result))
         
         
-
 
 docdate = datetime.datetime.combine(datetime.datetime.now().date(),datetime.time())
 
-#docdate = datetime.datetime.combine(datetime.date(year=2020,month=1,day=1),datetime.time())
+docdate = datetime.datetime.combine(datetime.date(year=2020,month=5,day=25),datetime.time())
 
 sensor_id = 'LOUNGE_AMBIENT'
-
-coll = db.get_collection('sensors.%s'%sensor_id)
+stype = 'temp'
+coll = db.get_collection('sensors.%s.%s'%(stype,sensor_id))
 print "coll repr is %s"%repr(coll)
 
 doc = new_sensor_day_doc(sensor_id, docdate)
 #print "doc is %s"%repr(doc)
-#doc =  { 'tag' : 'L1_AMB' , 'temp' : 23.43 , 'date' : '2017-11-20T00:12:42.232945' }
+doc =  { 'tag' : 'L1_AMB' , 'temp' : 23.43 , 'date' : '2017-11-20T00:12:42.232945' }
 
 
 
@@ -107,4 +89,4 @@ doc = new_sensor_day_doc(sensor_id, docdate)
 #IOLoop.current().run_sync(lambda: do_update_doc(collection=coll,doc=doc, docdate=docdate, hour=0, minute=0))
 
 
-IOLoop.current().run_sync(lambda: do_insert(sensor_id=sensor_id, docdate=docdate, hour=0, minute=0, value=45.76))
+IOLoop.current().run_sync(lambda: do_insert(sensor_id=sensor_id, stype=stype, docdate=docdate, hour=0, minute=0, value=255.55))
