46a885b7add8b5edad4064cce96c895aaa04cd9e
Steve Murphy <steve@gnusys.com>
2018-01-11 14:59:41 +0100

    device files for rf

---
+++ arch/cc430/devices/rf/device.c
@@ -0,0 +1,33 @@
+/******************************************************************************
+*
+* Copyright (c) 2012-16 Gnusys Ltd
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+******************************************************************************/
+
+
+#include "mrf_if.h"
+#include "device.h"
+static IF_STATUS _if_status[NUM_INTERFACES];
+static MRF_PKT_HDR _if_ackbuffs[NUM_INTERFACES];
+
+
+//extern const MRF_IF_TYPE mrf_uart_cc_if;
+extern const MRF_IF_TYPE mrf_rf_cc_if;
+
+const MRF_IF _sys_ifs[] = {
+  [RF0]   = {&_if_status[RF0], &mrf_rf_cc_if  , &_if_ackbuffs[RF0]}  
+};
+
---
+++ arch/cc430/devices/rf/device.h
@@ -0,0 +1,34 @@
+/******************************************************************************
+*
+* Copyright (c) 2012-16 Gnusys Ltd
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+* 
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+******************************************************************************/
+
+#ifndef __DEVICE_INCLUDED__
+#define __DEVICE_INCLUDED__
+
+
+// device type usbrf - one usb and one rf interface
+
+typedef  enum { RF0,
+                NUM_INTERFACES} I_F;
+
+// 8 buffs for allocation by sys
+#define _MRF_BUFFS 8
+
+#define IQUEUE_DEPTH 16
+
+#endif
